dc363f476a6cd6caa2fe204d342ed0df5675384d
lukechang24 <lukechang244@gmail.com>
2020-03-29 03:56:59 -0700

    change

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const Firestore = require("@google-cloud/firestore")
 
 admin.initializeApp({
     credential: admin.credential.cert({
-        "projectId": process.env.FIREBASE_PROJECT_ID,
+        "project_id": process.env.FIREBASE_PROJECT_ID,
         "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
     }),
