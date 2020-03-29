e9a76a4c3dd6b1d890bfe1a4cdc9197594d850d2
lukechang24 <lukechang244@gmail.com>
2020-03-29 14:57:49 -0700

    finished project

--- commands/create.js
+++ commands/create.js
@@ -1,30 +1,30 @@
-// require("dotenv").config()
+require("dotenv").config()
 
-// module.exports = {
-//     name: "!create",
-//     description: "Creates a inhouse lobby",
-//     execute(msg, args, bot, firebase) {
-//         firebase.findUser(msg.author.id).get()
-//             .then(user => {
-//                 if(user.exists) {
-//                     firebase.findCurrentGame().get()
-//                         .then(game => {
-//                             if(game.size === 0) {
-//                                 firebase.createGame({
-//                                     players: [],
-//                                     team1: [],
-//                                     team2: [],
-//                                     winner: null,
-//                                     creator: msg.author.id
-//                                 })
-//                                 msg.channel.send("```Inhouse lobby created. Type '!queue' to join the game, or '!cancel' to drop this inhouse```")
-//                             } else {
-//                                 msg.reply("A game is already being hosted by...")
-//                             }
-//                         })
-//                 } else {
-//                     msg.reply("You have not registered yet. Type '!register' to do so.")
-//                 }
-//             })
-//     }
-// }
+module.exports = {
+    name: "!create",
+    description: "Creates a inhouse lobby",
+    execute(msg, args, bot, firebase) {
+        firebase.findUser(msg.author.id).get()
+            .then(user => {
+                if(user.exists) {
+                    firebase.findCurrentGame().get()
+                        .then(game => {
+                            if(game.size === 0) {
+                                firebase.createGame({
+                                    players: [],
+                                    team1: [],
+                                    team2: [],
+                                    winner: null,
+                                    creator: msg.author.id
+                                })
+                                msg.channel.send("```Inhouse lobby created. Type '!queue' to join the game, or '!cancel' to drop this inhouse```")
+                            } else {
+                                msg.reply("A game is already being hosted by...")
+                            }
+                        })
+                } else {
+                    msg.reply("You have not registered yet. Type '!register' to do so.")
+                }
+            })
+    }
+}
--- commands/matchHistory.js
+++ commands/matchHistory.js
@@ -1,41 +1,41 @@
-// require("dotenv").config()
+require("dotenv").config()
 
-// module.exports = {
-//     name: "!matchhistory",
-//     description: "Displays the last 5(default) games",
-//     execute(msg, args, bot, firebase) {
-//         firebase.findMatchHistory().orderBy("date", "desc").limit(typeof parseInt(args[0]) === "number" && args[0] <= 10 ? parseInt(args[0]) : 5).get()
-//             .then(async games => {
-//                 let matchHistory = ""
-//                 let count = 0;
-//                 let matches = []
-//                 await games.forEach(game => {
-//                     matches.push(game.data())
-//                 })
+module.exports = {
+    name: "!matchhistory",
+    description: "Displays the last 5(default) games",
+    execute(msg, args, bot, firebase) {
+        firebase.findMatchHistory().orderBy("date", "desc").limit(typeof parseInt(args[0]) === "number" && args[0] <= 10 ? parseInt(args[0]) : 5).get()
+            .then(async games => {
+                let matchHistory = ""
+                let count = 0;
+                let matches = []
+                await games.forEach(game => {
+                    matches.push(game.data())
+                })
 
-//                 for(let i = 0; i < matches.length; i++) {
-//                     matchHistory += `${matches[i].date.toDate()} \nTEAM 1 ${matches[i].winner === "team1" ? "🥇" : ""}               TEAM 2 ${matches[i].winner === "team2" ? "🥇" : ""}\n`
-//                     let matchInfo = ""
-//                     for(let i = 0; i < matches[i].team1.length; i++) {
-//                         try {
-//                             const username1 = await bot.users.fetch(matches[i].team1[i].id).then(user => {
-//                                 return user.username
-//                             })
-//                             const username2 = await bot.users.fetch(matches[i].team2[i].id).then(user => {
-//                                 return user.username
-//                             })
-//                             matchInfo += `${username1}               ${username2} \n`
-//                             if(i === matches[i].team1.length-1) {
-//                                 matchInfo += "\n"
-//                             }
-//                         } catch (error) {
-//                             console.log(error)
-//                         }
-//                     }
-//                     matchHistory += matchInfo
-//                 }
+                for(let i = 0; i < matches.length; i++) {
+                    matchHistory += `${matches[i].date.toDate()} \nTEAM 1 ${matches[i].winner === "team1" ? "🥇" : ""}               TEAM 2 ${matches[i].winner === "team2" ? "🥇" : ""}\n`
+                    let matchInfo = ""
+                    for(let i = 0; i < matches[i].team1.length; i++) {
+                        try {
+                            const username1 = await bot.users.fetch(matches[i].team1[i].id).then(user => {
+                                return user.username
+                            })
+                            const username2 = await bot.users.fetch(matches[i].team2[i].id).then(user => {
+                                return user.username
+                            })
+                            matchInfo += `${username1}               ${username2} \n`
+                            if(i === matches[i].team1.length-1) {
+                                matchInfo += "\n"
+                            }
+                        } catch (error) {
+                            console.log(error)
+                        }
+                    }
+                    matchHistory += matchInfo
+                }
 
-//                 msg.reply(`\`\`\`${matchHistory}\`\`\``)
-//             })
-//     }
-// }
+                msg.reply(`\`\`\`${matchHistory}\`\`\``)
+            })
+    }
+}
