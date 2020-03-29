8315a6628d969b548517cc800e68e0c48b50fbce
lukechang24 <lukechang244@gmail.com>
2020-03-29 15:49:56 -0700

    done

--- commands/commands.js
+++ commands/commands.js
@@ -4,6 +4,6 @@ module.exports = {
     name: "!commands",
     description: "Lists the commands for Inhouse-Bot",
     execute(msg, args, bot, firebase) {
-        msg.reply("\n**!register** - registers you into Inhouse-Bot's database \n**!create** - creates an inhouse lobby \n**!queue** - Places you into the inhouse lobby \n**!randomize** - randomizes the teams by average elo (L) \n**!winner team[x]** - Records the inhouse game results depending on which team that won (L) \n**!stats** - Displays your stats, or alternatively, the player whose username you type in front of the command \n**!matchHistory** - Displays the last 5 inhouse matches, or alternatively, up to the number you type in front of the command \n\n*(L) - Only usable by room creator*")
+        msg.reply("\n**!register** - registers you into Inhouse-Bot's database \n**!create** - creates an inhouse lobby \n**!queue** - places you into the inhouse lobby \n**!randomize** - randomizes the teams by average elo \\*\\* \n**!winner team[x]** - records the inhouse game results depending on which team that won \\*\\* \n**!cancel** - cancels the current inhouse queue\n**!stats** - displays your stats, or alternatively, the player whose username you type in front of the command \n**!matchHistory** - displays the last 5 inhouse matches, or alternatively, up to the number you type in front of the command \n\n*\*\* These commands are only usable by room creator*")
     }
 }
